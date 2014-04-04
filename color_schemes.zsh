ee7c9a7d45cd39820fa35fcccec015f6a31ea228
Josh Tolle (jstolle) <josh@jstolle.com>
2014-04-03 20:26:04 -0500

    Added a bunch of things

---
+++ color_schemes.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+
+###
+# I continuously have to remember how to get color schemes into different
+# things.  This is to help me remember.
+#
+# Unfortunately, Zsh arrays start at 1 instead of 0, so the first color in
+# the array is 1, but it should be assigned to color0.
+###
+
+## Zenburn
+MY_COLORS=('#1E2320' '#705050' '#60b48a' '#dfaf8f' '#506070' '#dc8cc3' '#8cd0d3' '#dcdccc' '#709080' '#dca3a3' '#c3bf9f' '#f0dfaf' '#94bff3' '#ec93d3' '#93e0e3' '#ffffff')
